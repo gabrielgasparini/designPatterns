a6731a7dcfd8cc9088386675af8fb1e751547538
felipe passada <felipepassada@gmail.com>
2018-05-28 20:36:08 -0300

    Adicao dos getters e setters da classe, e adicao do atributo do tipo Usuario

--- src/java/ifsp/pwe/Beans/Produto.java
+++ src/java/ifsp/pwe/Beans/Produto.java
@@ -14,6 +14,47 @@ public class Produto {
     private String  nome;
     private String  descricao;
     private float   valor;
+    private Usuario usuario;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public float getValor() {
+        return valor;
+    }
+
+    public void setValor(float valor) {
+        this.valor = valor;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
     
     
 }
