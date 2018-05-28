77a80b8a56aa00f861d5a30e435c14f2c5973913
felipe passada <felipepassada@gmail.com>
2018-05-28 20:20:57 -0300

    Adicao da classe Produto

---
+++ src/java/ifsp/pwe/Beans/Produto.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ifsp.pwe.Beans;
+
+/**
+ *
+ * @author felipe
+ */
+public class Produto {
+    private Integer id;
+    private String  nome;
+    private String  categoria;
+    private float   valor;
+}
+
